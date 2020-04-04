167bc7954c22c67c876174243d380068fcf1a85f
hugo <hjbello.wk@gmail.com>
2020-04-04 11:26:17 +0200

    fix

--- extract-news-to-directory/saveToJson.js
+++ extract-news-to-directory/saveToJson.js
@@ -3,14 +3,19 @@ require('dotenv').config()
 const url = process.env["database_url"];
 const fs = require("fs");
 
+const pathout = "../data/json_news_tagged/"
+const pathoutbundle = "../data/json_bundle_reviews/"
+
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("reviews-scaping");
+  var dbo = db.db("reviews-scraping");
   var query = {};
   dbo.collection("Reviews").find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(url)
-    console.log(result);
+    let data = JSON.stringify(result);
+    fs.writeFileSync(pathoutbundle + "large-bundle.json", data);
+        
     db.close();
   });
 });
