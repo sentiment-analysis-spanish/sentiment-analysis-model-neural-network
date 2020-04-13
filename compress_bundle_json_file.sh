0adf461100d2b30883d3c8f11260efe36d8ca363
hugo <hjbello.wk@gmail.com>
2020-04-13 17:36:36 +0200

    new model added

--- compress_bundle_json_file.sh
+++
@@ -1,8 +0,0 @@
-cd data/json_bundle_reviews
-#gunzip -ck large-bundle-corona.json > reviews-180k-bundle-after-corona.json.gz
-zip reviews-500k-bundle-20200409.zip large-bundle-clean.json 
-zip reviews-500k-bundle-20200409-full-bundle.zip large-bundle.json 
-
-cd ..
-cd neural_network_config
-zip model_and_tokenizer-500k.zip model.h5 model.json temp-model.h5 tokenizer.pickle 
---
+++ compress_model_data.sh
@@ -0,0 +1,4 @@
+cd data/json_bundle_reviews
+cd ..
+cd neural_network_config
+zip model_and_tokenizer-600k.zip model.h5 model.json temp-model.h5 tokenizer.pickle 
