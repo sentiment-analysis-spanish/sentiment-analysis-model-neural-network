ec5c0b1182dcf786b415471988d2dca3b0ca7a07
Hugo J <hjbello.wk@gmail.com>
2020-10-21 00:02:22 +0200

    clean up

---
+++ sentiment_regression_neural_network/cleaner_n_grams.py
@@ -0,0 +1,84 @@
+#tar -cJf filename.tar.xz /path/to/folder_or_file ...
+#https://blog.mimacom.com/text-classification/
+
+#import nltk
+#from nltk.corpus import stopwords
+
+from keras.preprocessing.text import Tokenizer
+
+#nltk.download('stopwords')
+import pandas as pd
+import re
+import numpy as np
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+class Cleaner_ngrams:
+    def __init__(self):
+        self.REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+        self.BAD_SYMBOLS_RE = re.compile('[^\w\s]')
+        #self.STOPWORDS = set(stopwords.words('spanish'))
+        self.STOPWORDS = ['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con',  'una', 'su', 'al', 'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'sí', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'también', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mí', 'antes', 'algunos', 'qué', 'unos', 'yo', 'otro', 'otras', 'otra', 'él', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tú', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosotros', 'vosotras', 'os', 'mío', 'mía', 'míos', 'mías', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estás', 'está', 'estamos', 'estáis', 'están', 'esté', 'estés', 'estemos', 'estéis', 'estén', 'estaré', 'estarás', 'estará', 'estaremos', 'estaréis', 'estarán', 'estaría', 'estarías', 'estaríamos', 'estaríais', 'estarían', 'estaba', 'estabas', 'estábamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuviéramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviésemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad', 'he', 'has', 'ha', 'hemos', 'habéis', 'han', 'haya', 'hayas', 'hayamos', 'hayáis', 'hayan', 'habré', 'habrás', 'habrá', 'habremos', 'habréis', 'habrán', 'habría', 'habrías', 'habríamos', 'habríais', 'habrían', 'había', 'habías', 'habíamos', 'habíais', 'habían', 'hube', 'hubiste', 'hubo', 'hubimos', 'hubisteis', 'hubieron', 'hubiera', 'hubieras', 'hubiéramos', 'hubierais', 'hubieran', 'hubiese', 'hubieses', 'hubiésemos', 'hubieseis', 'hubiesen', 'habiendo', 'habido', 'habida', 'habidos', 'habidas', 'soy', 'eres', 'es', 'somos', 'sois', 'son', 'sea', 'seas', 'seamos', 'seáis', 'sean', 'seré', 'serás', 'será', 'seremos', 'seréis', 'serán', 'sería', 'serías', 'seríamos', 'seríais', 'serían', 'era', 'eras', 'éramos', 'erais', 'eran', 'fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron', 'fuera', 'fueras', 'fuéramos', 'fuerais', 'fueran', 'fuese', 'fueses', 'fuésemos', 'fueseis', 'fuesen', 'sintiendo', 'sentido', 'sentida', 'sentidos', 'sentidas', 'siente', 'sentid', 'tengo', 'tienes', 'tiene', 'tenemos', 'tenéis', 'tienen', 'tenga', 'tengas', 'tengamos', 'tengáis', 'tengan', 'tendré', 'tendrás', 'tendrá', 'tendremos', 'tendréis', 'tendrán', 'tendría', 'tendrías', 'tendríamos', 'tendríais', 'tendrían', 'tenía', 'tenías', 'teníamos', 'teníais', 'tenían', 'tuve', 'tuviste', 'tuvo', 'tuvimos', 'tuvisteis', 'tuvieron', 'tuviera', 'tuvieras', 'tuviéramos', 'tuvierais', 'tuvieran', 'tuviese', 'tuvieses', 'tuviésemos', 'tuvieseis', 'tuviesen', 'teniendo', 'tenido', 'tenida', 'tenidos', 'tenidas', 'tened']
+        self.tokenizer = None
+    
+
+
+    def clean_text(self, text):
+        text = text.lower() # lowercase text
+        text = self.REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+        text = self.BAD_SYMBOLS_RE.sub('', text) # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
+    #    text = re.sub(r'\W+', '', text)
+        text = ' '.join(word for word in text.split() if word not in self.STOPWORDS) # remove stopwors from text
+        return text
+
+
+
+    def clean_news(self, df):
+        print("cleaning the text data for vectorizer")
+        df = df.reset_index(drop=True)
+        df['sentiment'] = df.apply (lambda row: self.categorize(row), axis=1)
+        df.dropna(subset=['content'], inplace=True)
+        df['content'] = df['content'].apply(self.clean_text)
+        df['content'] = df['content'].str.replace('\d+', '')
+        return df
+
+    def categorize (self, row):
+        if row['rate'] > 0.5 :
+            return 1
+        if row['rate'] < 0.5 :
+            return 0
+
+    def load_tokenizer(self, sentences):
+        print("loading CountVectorizer")
+        self.ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 2))
+        self.ngram_vectorizer.fit(sentences)
+
+        # saving tokenizer
+        with open('../data/neural_network_config/ngram_vectorizer.pickle', 'wb') as handle:
+            pickle.dump(self.ngram_vectorizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    def create_tokenizer_and_clean(self):
+        filename1 = "../data/json_bundle_reviews/positive_reviews.json"
+        filename2 = "../data/json_bundle_reviews/negative_reviews.json"
+        
+        
+        df1 = pd.read_json(filename1)
+        df2 = pd.read_json(filename2)
+
+        
+
+        self.df = df1.append(df2)
+
+        self.df = self.clean_news(self.df)
+        self.df.to_json("../data/json_bundle_reviews/large-bundle-clean.json",orient='records', force_ascii=False)
+        
+        sentences = self.df['content'].values
+        self.load_tokenizer(sentences)
+
+
+if __name__== "__main__":
+    cleaner = Cleaner_ngrams()
+    cleaner.create_tokenizer_and_clean()
